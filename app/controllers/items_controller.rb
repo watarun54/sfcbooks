ea236ae743c1fca42c6b2ca1b7902529ada0aa88
watarun <watarun54@gmail.com>
2020-01-02 02:30:20 +0900

    fix color desing and add show.html.erb of items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+
   def index
     @items = Item.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
