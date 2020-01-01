4ae84941a0b97a06a58ab5a4e33f08e24b9a11d2
watarun <watarun54@gmail.com>
2020-01-02 03:21:01 +0900

    dev-1: rails g devise User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'admin', email: 'admin@test.com', password: 'password')
+User.create(name: 'satou', email: 'satou@test.com', password: 'password')
+User.create(name: 'suzuki', email: 'suzuki@test.com', password: 'password')
+User.create(name: 'tanaka', email: 'tanaka@test.com', password: 'password')
+User.create(name: 'test',email: 'test@test.com', password: 'password')
+
+Item.create(title: 'Sample1', status: "出品中", price: 1000, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
+Item.create(title: 'Sample2', status: "制約済", price: 1500, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
+Item.create(title: 'Sample3', status: "出品中", price: 500, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
