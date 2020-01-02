aefb9ab0e287e994880a2b63bffa225e54552661
watarun <watarun54@gmail.com>
2020-01-02 18:47:10 +0900

    dev-1: fix design of sign up page and alert

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module Sfcbooks
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
