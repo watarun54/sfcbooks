cbd7984219a8cd18458b12095906d5b8d6e38fc2
watarun <watarun54@gmail.com>
2020-01-04 00:15:45 +0900

    dev-4: feature messages management page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,15 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_room, only: [:show]
+  before_action :authenticate_user_for_rooms!, only: [:show]
+
+  def index
+    @rooms = Room.rooms_of(current_user.id)
+  end
 
   def show
-    @host_user = User.find(@room.host_user_id)
-    @guest_user = User.find(@room.guest_user_id)
     @message = Message.new
-    @messages = Message.where(user_id: [@host_user.id, @guest_user.id])
+    @messages = @room.messages
   end
 
   def create
@@ -23,5 +26,11 @@ class RoomsController < ApplicationController
 
   def set_room
     @room = Room.find(params[:id])
+  end
+
+  def authenticate_user_for_rooms!
+    @host_user = User.find(@room.host_user_id)
+    @guest_user = User.find(@room.guest_user_id)
+    redirect_to root_path, notice: "You can't enter the room." if current_user != @host_user && current_user != @guest_user
   end
 end
--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,6 @@
 module RoomsHelper
+  def host_or_guest?(room)
+    return "Host" if room.host_user_id == current_user.id
+    return "Guest" if room.guest_user_id == current_user.id
+  end
 end
