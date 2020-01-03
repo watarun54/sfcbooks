32b62aee12c6df012d0dbbc7be326788b3e62c20
watarun <watarun54@gmail.com>
2020-01-03 12:56:22 +0900

    dev-2: add validations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :status, presence: true, length: { maximum: 50 }
+  validates :price, presence: true, numericality: { only_integer: true, less_than: 100_000 }
+  validates :lecture, length: { maximum: 50 }
+  validates :teacher, length: { maximum: 50 }
+  validates :memo, length: { maximum: 500 }
 end
