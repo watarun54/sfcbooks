c076c1171624a34078cb4ff75e8cd5ef9e532ce2
watarun <watarun54@gmail.com>
2020-01-04 13:06:35 +0900

    dev-5: add validation of image size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  def size_range
+    1..5.megabytes
+  end
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
