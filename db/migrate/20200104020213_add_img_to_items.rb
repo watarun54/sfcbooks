08342d81e098d1d3d9f351b8d8fa4cf9819fc394
watarun <watarun54@gmail.com>
2020-01-04 11:32:19 +0900

    dev-5: generate image model

--- db/migrate/20200104020213_add_img_to_items.rb
+++
@@ -1,7 +0,0 @@
-class AddImgToItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :img1, :string
-    add_column :items, :img2, :string
-    add_column :items, :img3, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_020213) do
+ActiveRecord::Schema.define(version: 2020_01_04_023034) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "path"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -22,9 +30,6 @@ ActiveRecord::Schema.define(version: 2020_01_04_020213) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.string "img1"
-    t.string "img2"
-    t.string "img3"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -60,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_01_04_020213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "items"
   add_foreign_key "items", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
