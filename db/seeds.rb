755012d6abdeb4d2d10564f02d7fc92c5c9371a2
watarun <watarun54@gmail.com>
2020-01-05 12:10:30 +0900

    dev-7: fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
-User.create(name: 'admin', email: 'admin@test.com', password: 'password')
-User.create(name: 'satou', email: 'satou@test.com', password: 'password')
-User.create(name: 'suzuki', email: 'suzuki@test.com', password: 'password')
-User.create(name: 'tanaka', email: 'tanaka@test.com', password: 'password')
-User.create(name: 'test',email: 'test@test.com', password: 'password')
+# rails db:seed
+admin = User.create(name: 'admin', email: 'admin@test.com', password: 'password')
+satou = User.create(name: 'satou', email: 'satou@test.com', password: 'password')
+suzuki = User.create(name: 'suzuki', email: 'suzuki@test.com', password: 'password')
+tanaka = User.create(name: 'tanaka', email: 'tanaka@test.com', password: 'password')
+test1 = User.create(name: 'test1',email: 'test1@test.com', password: 'password')
+test2 = User.create(name: 'test2',email: 'test2@test.com', password: 'password')
 
-User.first.items.create(title: 'Sample1', status: 0, price: 1000, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
-User.first.items.create(title: 'Sample2', status: 1, price: 1500, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
-User.first.items.create(title: 'Sample3', status: 2, price: 500, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
-User.second.items.create(title: 'Sample4', status: 0, price: 1000, lecture: "論理学", teacher: "小泉進次郎", memo: "全体的に良好な状態です。")
-User.second.items.create(title: 'Sample5', status: 1, price: 1500, lecture: "論理学", teacher: "小泉進次郎", memo: "全体的に良好な状態です。")
-User.second.items.create(title: 'Sample6', status: 2, price: 500, lecture: "論理学", teacher: "小泉進次郎", memo: "全体的に良好な状態です。")
+[admin, satou, suzuki, tanaka, test1, test2].each do |user|
+  user.items.create(title: "Book1 of #{user.name}", status: 0, price: 1000, lecture: "心のオシャレ学", teacher: "田中太郎", memo: "表紙に若干のイタミあり。")
+  user.items.create(title: "Book2 of #{user.name}", status: 1, price: 1500, lecture: "論理学", teacher: "小泉進次郎", memo: "全体的に良好な状態です。")
+  user.items.create(title: "Book3 of #{user.name}", status: 2, price: 1800, lecture: "統計学", teacher: "福沢諭吉", memo: "値下げ交渉可能。")
+end
