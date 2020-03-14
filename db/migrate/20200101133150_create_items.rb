34568254eb56739333a0a7654bfaa35227b47254
watarun <watarun54@gmail.com>
2020-03-14 20:55:12 +0900

    dev-14: fix migration files

--- db/migrate/20200101133150_create_items.rb
+++ db/migrate/20200101133150_create_items.rb
@@ -2,8 +2,8 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :title
-      t.string :status
-      t.integer :price
+      t.integer :status, default: 0
+      t.integer :price, default: 0
       t.string :lecture
       t.string :teacher
       t.text :memo
