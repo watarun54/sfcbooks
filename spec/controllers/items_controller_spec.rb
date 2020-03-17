0bc9a83a72b938f2e7506414e0f5fc4e558dc9e7
watarun <watarun54@gmail.com>
2020-03-17 20:53:30 +0900

    feature spec of create/update/destroy methods of items_controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 describe ItemsController do
-  let(:item) { create(:item) }
-  let(:items) { create_list(:item, 2) }
+  let(:user) { create(:user) }
+  let(:item) { create(:item, user: user) }
+  let(:items) { create_list(:item, 3) }
 
   describe "GET #index" do
     before { get :index, params: {}, session: {} }
@@ -10,11 +11,9 @@ describe ItemsController do
     it 'has a 200 status code' do
       expect(response).to have_http_status(:ok)
     end
-
     it 'assigns @items' do
       expect(assigns(:items)).to match_array items
     end
-
     it 'renders the :index template' do
       expect(response).to render_template :index
     end
@@ -26,22 +25,66 @@ describe ItemsController do
     it "has a 200 status code" do
       expect(response).to have_http_status(:ok)
     end
-
     it "assigns @item" do
       expect(assigns(:item)).to eq item
     end
-
     it "renders the :show template" do
       expect(response).to render_template :show
     end
   end
 
   describe "POST #create" do
+    before { login(user) }
+
+    it "saves a new item" do
+      expect {
+        post :create, params: { item: attributes_for(:item) }
+      }.to change(Item, :count).by(1)
+    end
+    it "redirects to /items/:id" do
+      post :create, params: { item: attributes_for(:item) }
+      expect(response).to redirect_to "/items/#{assigns(:item).id}"
+    end
   end
 
   describe "PATCH #update" do
+    before {
+      login(user)
+      get :edit, params: { id: item }, session: {}
+    }
+
+    it "assigns @item" do
+      expect(assigns(:item)).to eq item
+    end
+    it "changes @item's attributes" do
+        patch :update, params: { id: item, item: attributes_for(:item, title: 'updated test', price: 1500) }
+        item.reload
+        expect(item.title).to eq "updated test"
+        expect(item.price).to eq 1500
+    end
+    it "redirects to /items/:id" do
+      post :create, params: { item: attributes_for(:item) }
+      expect(response).to redirect_to "/items/#{assigns(:item).id}"
+    end
   end
 
   describe "DELETE #destroy" do
+    before {
+      login(user)
+      get :edit, params: { id: item }, session: {}
+    }
+
+    it "assigns @item" do
+      expect(assigns(:item)).to eq item
+    end
+    it "destroys the item" do
+      expect {
+        delete :destroy, params: { id: item }
+      }.to change(Item,:count).by(-1)
+    end
+    it "redirects to /items/manage" do
+      delete :destroy, params: { id: item }
+      expect(response).to redirect_to manage_items_path
+    end
   end
 end
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,5 @@
+module ControllerMacros
+  def login(user)
+    sign_in user
+  end
+end
