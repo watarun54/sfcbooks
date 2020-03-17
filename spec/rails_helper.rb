55c5dd4ce10ceb9a548225ba67752db8bb5b911e
watarun <watarun54@gmail.com>
2020-03-17 22:38:29 +0900

    dev-13: fix rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 end
