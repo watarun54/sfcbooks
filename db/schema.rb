28ee5fee99e5f96bfbab43396204d596dd7eb9a4
watarun <watarun54@gmail.com>
2020-03-26 18:02:32 +0900

    dev-24: feature read/unread function for messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_044456) do
+ActiveRecord::Schema.define(version: 2020_03_26_074743) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "path"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_044456) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_read", default: false, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
