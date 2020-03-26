6535dad2f248615f603e92a585714ea0db2898e2
watarun <watarun54@gmail.com>
2020-03-26 14:48:45 +0900

    dev-22: create room with at least one message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,33 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.messages.new(message_params)
-    @room = Room.find(@message.room_id)
+    is_success = true
+
+    begin
+      ActiveRecord::Base.transaction do
+        if params[:message][:room_id].present?
+          @room = Room.find(@message.room_id)
+        else
+          # 新規メッセージの場合は、Roomを作成する
+          @room = Room.create!(
+            item_id: params[:item_id],
+            host_user_id: params[:host_user_id],
+            guest_user_id: params[:guest_user_id]
+          )
+          @message.room_id = @room.id
+        end
+        @message.save!
+      end
+    rescue => e
+      is_success = false
+      redirect_path = @room.id ? @room : new_room_path(params: {item_id: params[:item_id]})
+    end
 
     respond_to do |format|
-      if @message.save
+      if is_success
         format.html { redirect_to @room, notice: 'Message was successfully created.' }
       else
-        format.html { redirect_to @room, notice: @message.errors.full_messages.first }
+        format.html { redirect_to redirect_path, notice: @message.errors.full_messages.first }
       end
     end
   end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :item
-  has_many :messages, dependent: :destroy
+  has_many :messages, dependent: :delete_all
 
   scope :rooms_of, -> (user_id) { where(host_user_id: user_id).or(where(guest_user_id: user_id)) }
 end
